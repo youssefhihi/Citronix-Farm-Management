6d0077334ed8589c5b1126f31926037c57ed0b32
youssefhihi <youssefhihi18@gmail.com>
2024-11-17 16:07:21 +0100

    edit and delete a farm CIT-7

--- src/main/java/com/ys/citronix/farmManagement/application/mapper/FarmMapper.java
+++ src/main/java/com/ys/citronix/farmManagement/application/mapper/FarmMapper.java
@@ -6,9 +6,12 @@ import com.ys.citronix.farmManagement.application.dto.request.FarmRequestUpdateD
 import com.ys.citronix.farmManagement.application.dto.response.FarmResponseDto;
 import com.ys.citronix.farmManagement.domain.model.Farm;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 
 @Mapper(componentModel = "spring")
 public interface FarmMapper {
+    @Mapping(target = "createdDate" ,expression = "java(java.time.LocalDateTime.now())")
     Farm toEntity(FarmRequestDto farmRequestDto);
     Farm toEntity(FarmRequestUpdateDto farmRequestUpdateDto);
     FarmResponseDto toDto(Farm farm);
--- src/main/java/com/ys/citronix/farmManagement/domain/service/FarmService.java
+++ src/main/java/com/ys/citronix/farmManagement/domain/service/FarmService.java
@@ -4,7 +4,6 @@ package com.ys.citronix.farmManagement.domain.service;
 import com.ys.citronix.farmManagement.application.dto.request.FarmRequestDto;
 import com.ys.citronix.farmManagement.application.dto.request.FarmRequestUpdateDto;
 import com.ys.citronix.farmManagement.application.dto.response.FarmResponseDto;
-import com.ys.citronix.farmManagement.domain.valueObject.FarmId;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,5 +12,5 @@ public interface FarmService {
     List<FarmResponseDto> getAllFarms();
     FarmResponseDto createFarm(FarmRequestDto farmRequestDto);
     FarmResponseDto updateFarm(FarmRequestUpdateDto farmRequestDto);
-    Boolean deleteFarm(UUID farmId);
+    void deleteFarm(UUID farmId);
 }
