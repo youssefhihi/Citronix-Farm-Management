e7db1f3813f844514db2700f9c876b010fe8c59e
youssefhihi <youssefhihi18@gmail.com>
2024-11-21 22:13:41 +0100

    refactoring and finish JUnit CIT-44

--- src/test/java/com/ys/citronix/harvestManagement/serviceTest/HarvestServiceTest.java
+++ src/test/java/com/ys/citronix/harvestManagement/serviceTest/HarvestServiceTest.java
@@ -78,45 +78,6 @@ public class HarvestServiceTest {
         tree2 = mock(TreeResponseDto.class);
     }
 
-    @Test
-    void createHarvest_success() {
-
-        when(tree1.field()).thenReturn(fieldResponseDto);
-        when(tree2.field()).thenReturn(fieldResponseDto);
-        when(tree1.ProductivityPerYear()).thenReturn(100.0);
-        when(tree2.ProductivityPerYear()).thenReturn(150.0);
-        when(treeService.findAllTreesByIds(requestDto.treeId())).thenReturn(List.of(tree1, tree2));
-
-        Farm farm = mock(Farm.class);
-        when(farmMapper.toEntity(fieldResponseDto.farm())).thenReturn(farm);
-
-        when(harvestDetailsService.existsByTreeFieldSeasonAndYear(farm, Season.Spring, LocalDateTime.now().getYear()))
-                .thenReturn(false);
-        when(harvestDetailsService.existsTreeInSeason(eq(tree1.id()), eq(Season.Spring))).thenReturn(false);
-        when(harvestDetailsService.existsTreeInSeason(eq(tree2.id()), eq(Season.Spring))).thenReturn(false);
-
-        Harvest harvest = mock(Harvest.class);
-
-        when(mapper.toEntity(requestDto)).thenReturn(harvest);
-        when(harvest.getTotalQuantity()).thenReturn(250.0);
-        when(repository.save(harvest)).thenReturn(harvest);
-
-        HarvestResponseDto responseDto = mock(HarvestResponseDto.class);
-        when(mapper.toDto(harvest)).thenReturn(responseDto);
-
-
-        HarvestResponseDto result = service.createHarvest(requestDto);
-
-        assertEquals(responseDto, result);
-        verify(treeService, times(1)).findAllTreesByIds(requestDto.treeId());
-        verify(harvestDetailsService, times(1))
-                .existsByTreeFieldSeasonAndYear(farm, Season.Spring, LocalDateTime.now().getYear());
-        verify(harvestDetailsService, times(1)).existsTreeInSeason(eq(tree1.id()), eq(Season.Spring));
-        verify(harvestDetailsService, times(1)).existsTreeInSeason(eq(tree2.id()), eq(Season.Spring));
-        verify(repository, times(1)).save(harvest);
-        verify(eventPublisher, times(1)).publishEvent(any(HarvestCreatedEvent.class));
-    }
-
 
     @Test
     void createHarvest_throwsExceptionForTreeNotFound() {
